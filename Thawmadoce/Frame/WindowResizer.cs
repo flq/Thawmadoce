7b5b88e1ea63555ff6fbb872936ecba56896eab3
flq <fquednau@gmail.com>
2011-11-18 20:27:49 +0100

    Different soln, less code, looks roughly equally shitty but should be easier on the resources

--- Thawmadoce/Frame/WindowResizer.cs
+++ Thawmadoce/Frame/WindowResizer.cs
@@ -1,74 +1,42 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
-using System.Runtime.InteropServices;
-using System.Windows.Threading;
+using System.Windows.Interop;
 
 namespace Thawmadoce.Frame
 {
     /// <summary>
-    /// Inspired by http://www.codeproject.com/KB/WPF/WPF_Window_Resizing.aspx
-    /// but heavily modified.
+    /// Used from http://blog.kirupa.com/?p=256
     /// </summary>
     public class WindowResizer
     {
-        private readonly Window _target;
-        private bool _resizing;
-        private PointApi _resizePoint;
-        private Size _resizeSize;
-        private readonly DispatcherTimer _timer;
+        private const int WmSyscommand = 0x112;
+        private readonly HwndSource hwndSource;
+
+        public enum ResizeDirection
+        {
+            BottomRight = 8,
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 
         public WindowResizer(Window target, UIElement gripElement)
         {
-            _target = target;
-            
-            _timer = new DispatcherTimer(DispatcherPriority.Send, _target.Dispatcher)
-                         {Interval = TimeSpan.FromMilliseconds(10)};
-            _timer.Tick += HandleMouseCheck;
-            gripElement.MouseLeftButtonDown += HandleElementMouseLeftButtonDown;
+            hwndSource = PresentationSource.FromVisual(target) as HwndSource;
+            gripElement.PreviewMouseDown += HandleMouseDown;
         }
 
-        private void HandleElementMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void HandleMouseDown(object sender, MouseButtonEventArgs e)
         {
-            GetCursorPos(out _resizePoint);
-            _resizeSize = new Size(_target.Width, _target.Height);
-            _resizing = true;
-            _timer.Start();
+            ResizeWindow(ResizeDirection.BottomRight);
         }
 
-        private void HandleMouseCheck(object sender, EventArgs e)
+        private void ResizeWindow(ResizeDirection direction)
         {
-            if (_resizing)
-            {
-                UpdateSize();
-                UpdateMouseDown();
-            }
-            else
-                _timer.Stop();
-        }
-
-        private void UpdateSize()
-        {
-            PointApi p;
-            GetCursorPos(out p);
-            _target.Height = _resizeSize.Height - (_resizePoint.Y - p.Y);
-            _target.Width = _resizeSize.Width - (_resizePoint.X - p.X);
-        }
-
-        private void UpdateMouseDown()
-        {
-            if (Mouse.LeftButton == MouseButtonState.Released)
-                _resizing = false;
-        }
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool GetCursorPos(out PointApi lpPoint);
-
-        private struct PointApi
-        {
-            public int X;
-            public int Y;
+            SendMessage(hwndSource.Handle, WmSyscommand, (IntPtr)(61440 + direction), IntPtr.Zero);
         }
     }
+        
 }
